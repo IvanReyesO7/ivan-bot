b4386799d4be4f76794b80eda3c58b234eac6baf
Ivan Reyes <ivanwilf@hotmail.com>
2021-10-13 13:28:12 +0900

    Got id of Gemfile since I only have a dependency, fixed typo

--- messages.rb
+++ messages.rb
@@ -5,11 +5,11 @@ require_relative 'weather'
 class Message   
   def self.morning
     message = "*Good morning team!*" + "\n"
-    message += "Today is `#{(Date.today + 1).strftime('%b %d, %Y')}`" + "\n"
+    message += "Today is `#{(Date.today).strftime('%b %d, %Y')}`" + "\n"
     message += WeatherAPI.fetch_weather("tokyo") + "\n"
     message += "Let's give our best today 💪🏽" + "\n"
     random_fact = YAML.load_file('fun-facts.yml')["fun-facts"].sample
-    message += "*Fun fact of th day:* " + random_fact
+    message += "*Fun fact of the day:* " + random_fact
     return message
   end
 end
