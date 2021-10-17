1555f7b1a9f3f2b092962c384783a4c5caefad29
Ivan Reyes <ivanwilf@hotmail.com>
2021-10-17 10:36:55 +0900

    Update messages.rb

--- messages.rb
+++ messages.rb
@@ -5,7 +5,7 @@ require_relative 'weather'
 class Message   
   def self.morning
     message = "*Hello team!*" + "\n"
-    message += "Today is `#{(Date.today + 1).strftime('%b %d, %Y')}`" + "\n"
+    message += "Today is `#{(Date.today ).strftime('%b %d, %Y')}`" + "\n"
     message += WeatherAPI.fetch_weather("tokyo") + "\n"
     message += "Let's give our best today 💪🏽" + "\n"
     random_fact = YAML.load_file('fun-facts.yml')["fun-facts"].sample
